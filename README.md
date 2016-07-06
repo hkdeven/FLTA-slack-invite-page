c1bf1a91ddc380b3e34ab60da19dd9d21eda4bdf
Deven Blackburn <deven@codefll.org>
2016-07-06 10:32:08 -0400

    Inititial commit.  Fully functional and tested.  Ready for production.

---
+++ README.md
@@ -0,0 +1,25 @@
+slack-invites
+===================
+
+**This project is built on the [Stamplay](https://stamplay.com) platform to show you how to automate signups to your [Slack](https://slack.com/) community in just 30 minutes.**
+
+*Slack is a collaborative communication tool, widely used by teams and communities to exchange messages, files, links, and more. Its user base has grown rapidly during the last two years and here at Stamplay we use it daily as our main method of communication.*
+
+It’s fair to say, Stamplay loves Slack. We started using it for our internal communications, but it didn’t take too long before we realized that we could use it as a community platform too.
+
+Unfortunately, Slack doesn’t allow people to sign up directly. Instead, the Slack channel’s admin needs to invite them manually.
+
+In this tutorial we’ll show how to build your own fully automated Slack signup page. You’ll be able to point users to this page so they can enter their email address and receive an invite.
+
+You’ll learn some important features of Stamplay, including how to:
+
+* integrate Slack APIs
+* create and configure a Webhook
+* create and configure tasks
+* use Stamplay web hosting
+
+[Here’s what the final result will look like](https://slackinvites.stamplayapp.com/) – not bad for less than 30 minutes’ work!
+
+![picture alt](https://blog.stamplay.com/wp-content/uploads/2015/08/Screen-Shot-2015-08-24-at-17.06.26.png "Your automated Slack signup page")
+
+###[Click here to read the full tutorial](https://blog.stamplay.com/launch-your-community-with-a-fully-automated-slack-signup-page/)###
---
+++ assets/js/main.js
@@ -0,0 +1,111 @@
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+(function() {
+
+	"use strict";
+
+	// Methods/polyfills.
+
+		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
+			!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
+
+		// canUse
+			window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
+
+		// window.addEventListener
+			(function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
+
+	// Vars.
+		var	$body = document.querySelector('body');
+
+	// Disable animations/transitions until everything's loaded.
+		$body.classList.add('is-loading');
+
+		window.addEventListener('load', function() {
+			window.setTimeout(function() {
+				$body.classList.remove('is-loading');
+			}, 100);
+		});
+
+	// Signup Form.
+		(function() {
+
+			// Vars.
+				var $form = document.querySelectorAll('#signup-form')[0],
+					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+					$email = document.querySelectorAll('#signup-form #email')[0],
+					$message;
+
+					//prefill email value if any
+					$email.value = getParameterByName('email');
+
+
+			// Bail if addEventListener isn't supported.
+				if (!('addEventListener' in $form))
+					return;
+
+			// Message.
+				$message = document.createElement('span');
+					$message.classList.add('message');
+					$form.appendChild($message);
+
+				$message._show = function(type, text) {
+
+					$message.innerHTML = text;
+					$message.classList.add(type);
+					$message.classList.add('visible');
+
+					window.setTimeout(function() {
+						$message._hide();
+					}, 3000);
+
+				};
+
+				$message._hide = function() {
+					$message.classList.remove('visible');
+				};
+
+				Stamplay.init(stamplay_appid);
+			// Events.
+			// Note: If you're *not* using AJAX, get rid of this event listener.
+				$form.addEventListener('submit', function(event) {
+
+					event.stopPropagation();
+					event.preventDefault();
+
+					// Hide message.
+						$message._hide();
+
+					// Disable submit.
+						$submit.disabled = true;
+
+            var webhook = new Stamplay.Webhook(stamplay_webhookid);
+
+            var regExp = new RegExp("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$");
+						var valid = regExp.test($email.value);
+
+						if (valid) {
+							var data = { email: $email.value }
+							webhook.post(data).then(function (response) {
+								$form.reset();
+							  $submit.disabled = false;
+							  $message._show('success', 'The invite is on its way!');
+							});
+						} else {
+								$form.reset();
+							  $submit.disabled = false;
+							  $message._show('error', 'Please type a valid email address');
+						}
+
+				});
+
+		})();
+
+})();
